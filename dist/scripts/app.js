92c3f5845799f6d8feda0de87118ab22b5f06f34
Jack Schuss <jackschuss@gmail.com>
2016-05-30 13:07:42 -0400

    checkpoint3

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,1 +1,25 @@
-angular.module('jJams', []);
+(function() {
+  function config($stateProvider, $locationProvider) {
+    $locationProvider
+      .html5Mode({
+        enabled: true,
+        requireBase: false
+      });
+
+    $stateProvider
+      .state('landing', {
+        url: '/',
+        templateUrl: '/templates/landing.html'
+      })
+      .state('album', {
+        url: '/album',
+        templateUrl: '/templates/album.html'
+      });
+  }
+
+
+  angular
+    .module('jJams', ['ui.router'])
+    .config(config);
+})();
+
